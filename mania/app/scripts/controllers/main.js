e979a3387a295cbb5d3a2960822800c49f8fec64
Maria Poutachidou <maria.poutachidou@gmail.com>
2016-12-05 21:15:59 +0200

    Taking the data via http service from a json file

--- mania/app/scripts/controllers/main.js
+++ mania/app/scripts/controllers/main.js
@@ -8,26 +8,11 @@
  * Controller of the movieManiaApp
  */
 angular.module('movieManiaApp')
-  .controller('MainCtrl', function ($scope, $location) {
-    var movieList = [{
-      id: 1,
-      title: 'Usual Suspects',
-      image: 'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_SX214_AL_.jpg',
-      description: 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.',
-      category: 'Awesome',
-      slug: 'usual-suspects'
-    },
-    {
-      id: 2,
-      title: 'Star Wars',
-      image: 'http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX214_AL_.jpg',
-      description: 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire\'s world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.',
-      category: 'Epic',
-      slug: 'star-wars'
-    }];
-
-
-    $scope.movies = movieList;
+  .controller('MainCtrl', function ($scope, $location, $http) {
+    $http.get('/movies.json').success(function(data){
+      console.debug(data);
+      $scope.movies = data;
+    });
 
     $scope.movie = {
         title: '',
@@ -79,7 +64,4 @@ angular.module('movieManiaApp')
         return true;
     };
 
-  })
-  .controller('SubCtrl', function($scope){
-    $scope.title = 'Available to watch ' + $scope.movies.length + ' movies';
   });
